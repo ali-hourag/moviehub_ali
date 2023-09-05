34c98323fd1752d5358821cd37d76d61932a71ca
Ali Hourag <ali.hourag@gmail.com>
2023-09-05 17:50:46 +0200

    feat(clean code back)

--- server/src/middleware/checkJwt.middleware.ts
+++ server/src/middleware/checkJwt.middleware.ts
@@ -1,6 +1,6 @@
 import { auth } from "express-oauth2-jwt-bearer";
 import config from "../config/config";
-console.log("sii");
+
 export const checkJwtMiddleware = auth({
     audience: config.auth0.audience,
     issuerBaseURL: config.auth0.issuer,
